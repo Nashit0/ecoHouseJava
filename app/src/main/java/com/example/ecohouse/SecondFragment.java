ad056b40c29d8a118f1557c6adab6487e687ccd4
laugiern <nathan.laugier05@gmail.com>
2025-12-12 11:29:28 +0100

    bar de progression dans SecondFragment

--- app/src/main/java/com/example/ecohouse/SecondFragment.java
+++ app/src/main/java/com/example/ecohouse/SecondFragment.java
@@ -1,6 +1,7 @@
 package com.example.ecohouse;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +13,12 @@ import androidx.navigation.fragment.NavHostFragment;
 import com.example.ecohouse.databinding.FragmentSecondBinding;
 
 public class SecondFragment extends Fragment {
-
+    public Integer badPoints = 500;
+    public final Integer maxBadPoints = 1200;
+    public Integer score = 20;
     private FragmentSecondBinding binding;
+
+
 
     @Override
     public View onCreateView(
@@ -23,22 +28,23 @@ public class SecondFragment extends Fragment {
 
         binding = FragmentSecondBinding.inflate(inflater, container, false);
         return binding.getRoot();
-
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        //Code pour pouvoir modifier dynamiquement la barre de progression
-        View rectangleView = binding.rectangleView;
-        ViewGroup.LayoutParams params = rectangleView.getLayoutParams();
-        params.width = 20;   // largeur en pixels
-        params.height = 1250;  // hauteur en pixels
-        rectangleView.setLayoutParams(params);
-
-        binding.buttonSecond.setOnClickListener(v ->
+        binding.buttonSecond.setOnClickListener(v -> {
+            if (badPoints < maxBadPoints) {
+                badPoints+= 300;
+                Log.d("TEST_projet","test : "+badPoints);
+                updateProgBar();
+            }
+            else {
+                Log.d("TEST_projet", "limite atteinte");
                 NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment)
+                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+                }
+            }
         );
     }
 
@@ -48,4 +54,15 @@ public class SecondFragment extends Fragment {
         binding = null;
     }
 
+    public void updateProgBar() {
+        View rectangleView = binding.rectangleView;
+        int parentWidth = ((View) rectangleView.getParent()).getWidth();
+        Log.d("TEST_projet", "Taille écran : "+ parentWidth);
+
+        ViewGroup.LayoutParams params = rectangleView.getLayoutParams();
+        params.width =(badPoints * parentWidth) / maxBadPoints;
+        // largeur en pixels
+        params.height = 20;  // hauteur en pixels
+        rectangleView.setLayoutParams(params);
+    }
 }
